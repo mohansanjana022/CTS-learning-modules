d3613e49678703305269e85a45d12843d3f20758
mohansanjana022 <mohansanjana022@gmail.com>
2025-05-22 23:13:04 +0530

    Update StreamFilterExample.java

--- core java/StreamFilterExample.java
+++ core java/StreamFilterExample.java
@@ -1,1 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
+public class StreamFilterExample {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(3, 12, 7, 8, 5, 10, 6);
+
+        List<Integer> evenNumbers = numbers.stream()
+            .filter(n -> n % 2 == 0)
+            .collect(Collectors.toList());
+
+        evenNumbers.forEach(System.out::println);
+    }
+}
