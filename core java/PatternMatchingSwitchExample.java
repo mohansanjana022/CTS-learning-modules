ceb3a1cce57e1147f102acbb1acae047fea5d8d0
mohansanjana022 <mohansanjana022@gmail.com>
2025-05-22 23:22:07 +0530

    Update PatternMatchingSwitchExample.java

--- core java/PatternMatchingSwitchExample.java
+++ core java/PatternMatchingSwitchExample.java
@@ -1,1 +1,23 @@
+public class PatternMatchingSwitch {
 
+    public static void printObjectType(Object obj) {
+        String message = switch (obj) {
+            case Integer i -> "It's an Integer with value: " + i;
+            case String s -> "It's a String with length: " + s.length();
+            case Double d -> "It's a Double with value: " + d;
+            case Boolean b -> "It's a Boolean with value: " + b;
+            case null -> "The object is null";
+            default -> "Unknown type: " + obj.getClass().getSimpleName();
+        };
+        System.out.println(message);
+    }
+
+    public static void main(String[] args) {
+        printObjectType(42);
+        printObjectType("HELLO");
+        printObjectType(3.14);
+        printObjectType(true);
+        printObjectType(null);
+        printObjectType(new StringBuilder("Test"));
+    }
+}
