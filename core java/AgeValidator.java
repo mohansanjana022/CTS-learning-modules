c4656b99c2594ad6218a4dd538360bafd2d77062
mohansanjana022 <mohansanjana022@gmail.com>
2025-05-22 22:44:07 +0530

    Update AgeValidator.java

--- core java/AgeValidator.java
+++ core java/AgeValidator.java
@@ -1,1 +1,26 @@
+class InvalidAgeException extends Exception {
+    public InvalidAgeException(String message) {
+        super(message);
+    }
+}
+
+public class AgeValidator {
+    public static void checkAge(int age) throws InvalidAgeException {
+        if (age < 18) {
+            throw new InvalidAgeException("Invalid age: Must be 18 or older.");
+        } else {
+            System.out.println("Access granted. Age is valid.");
+        }
+    }
+
+    public static void main(String[] args) {
+        int userAge = 16;
+
+        try {
+            checkAge(userAge);
+        } catch (InvalidAgeException e) {
+            System.out.println("Exception caught: " + e.getMessage());
+        }
+    }
+}
 
