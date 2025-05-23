7602ced016a8c4f2ed4c9e9c990055e8b3f4406f
mohansanjana022 <mohansanjana022@gmail.com>
2025-05-23 15:53:09 +0530

    Update InactiveUsers.sql

--- ANSI SQL/InactiveUsers.sql
+++ ANSI SQL/InactiveUsers.sql
@@ -1,1 +1,11 @@
+SELECT 
+    u.user_id,
+    u.full_name
+FROM 
+    Users u
+LEFT JOIN 
+    Registration r ON u.user_id = r.user_id 
+    AND r.registration_date >= CURDATE() - INTERVAL 90 DAY
+WHERE 
+    r.user_id IS NULL;
 
