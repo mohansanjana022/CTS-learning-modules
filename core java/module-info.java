af4ddd19635b5efb8317617defbd9d9e0b07eba3
mohansanjana022 <mohansanjana022@gmail.com>
2025-05-23 14:46:49 +0530

    Update module-info.java

--- core java/module-info.java
+++ core java/module-info.java
@@ -1,1 +1,37 @@
+//com.utils
+
+  module com.utils {
+    exports com.utils;
+}
+
+//StringUtils.java
+  
+package com.utils;
+
+public class StringUtils {
+    public static String greet(String name) {
+        return "Hello, " + name + "!";
+    }
+}
+
+//com.greetings
+module com.greetings {
+    requires com.utils;
+}
+
+//Greeting.java
+package com.greetings;
+
+import com.utils.StringUtils;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String message = StringUtils.greet("this is the statement");
+        System.out.println(message);
+    }
+}
+//output
+javac -d com.greetings/out --module-path com.utils/out --module-source-path com.greetings/src $(find com.greetings/src -name "*.java")
+this is the statement
+  
 
