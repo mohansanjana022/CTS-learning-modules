dab2c6e27a4afe10084f8682c8671054bcc910bb
mohansanjana022 <mohansanjana022@gmail.com>
2025-05-23 16:03:32 +0530

    Update MostActiveCities.sql

--- ANSI SQL/MostActiveCities.sql
+++ ANSI SQL/MostActiveCities.sql
@@ -1,1 +1,13 @@
+SELECT 
+    u.city,
+    COUNT(DISTINCT r.user_id) AS unique_user_registrations
+FROM 
+    Users u
+JOIN 
+    Registration r ON u.user_id = r.user_id
+GROUP BY 
+    u.city
+ORDER BY 
+    unique_user_registrations DESC
+LIMIT 5;
 
