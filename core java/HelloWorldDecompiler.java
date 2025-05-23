06d2872ade6115ec1a11eac496423b7add46904c
mohansanjana022 <mohansanjana022@gmail.com>
2025-05-23 14:56:32 +0530

    Update HelloWorldDecompiler.java

--- core java/HelloWorldDecompiler.java
+++ core java/HelloWorldDecompiler.java
@@ -1,1 +1,14 @@
+public class HelloWorld {
+    public HelloWorld() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Decompiler!");
+    }
+}
+
+
+//after decompiling
+//$ java -jar cfr.jar HelloWorld.class
+//Hello, Decompiler!
 
