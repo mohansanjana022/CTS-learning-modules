785103957bf80c76a99d0a83111713a3f2f3533a
mohansanjana022 <mohansanjana022@gmail.com>
2025-05-25 13:41:39 +0530

    Update UnregisteredActiveUsers.sql

--- ANSI SQL/UnregisteredActiveUsers.sql
+++ ANSI SQL/UnregisteredActiveUsers.sql
@@ -1,1 +1,13 @@
+SELECT 
+    u.user_id,
+    u.full_name,
+    u.email,
+    u.registration_date
+FROM 
+    Users u
+LEFT JOIN 
+    Registration r ON u.user_id = r.user_id
+WHERE 
+    u.registration_date >= CURDATE() - INTERVAL 30 DAY
+    AND r.registration_id IS NULL;
 
