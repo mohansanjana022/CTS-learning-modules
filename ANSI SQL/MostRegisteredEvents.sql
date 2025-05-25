aca870739091cd88189a97641ef218f28bd96feb
mohansanjana022 <mohansanjana022@gmail.com>
2025-05-25 13:35:47 +0530

    Update MostRegisteredEvents.sql

--- ANSI SQL/MostRegisteredEvents.sql
+++ ANSI SQL/MostRegisteredEvents.sql
@@ -1,1 +1,14 @@
+SELECT 
+    e.event_id,
+    e.title,
+    COUNT(r.registration_id) AS total_registrations
+FROM 
+    Events e
+JOIN 
+    Registration r ON e.event_id = r.event_id
+GROUP BY 
+    e.event_id, e.title
+ORDER BY 
+    total_registrations DESC
+LIMIT 3;
 
