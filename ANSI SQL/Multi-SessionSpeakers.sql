0514696f5d964dfe15234d6418a0b3c7a4960490
mohansanjana022 <mohansanjana022@gmail.com>
2025-05-25 13:59:48 +0530

    Update Multi-SessionSpeakers.sql

--- ANSI SQL/Multi-SessionSpeakers.sql
+++ ANSI SQL/Multi-SessionSpeakers.sql
@@ -1,1 +1,12 @@
+INSERT INTO Sessions (event_id, title, speaker_name, start_time, end_time)
+VALUES (3, 'Advanced CSS', 'Bob Smith', '2025-07-02 14:00:00', '2025-07-02 16:00:00');
 
+SELECT 
+    speaker_name,
+    COUNT(*) AS session_count
+FROM 
+    Sessions
+GROUP BY 
+    speaker_name
+HAVING 
+    COUNT(*) > 1;
