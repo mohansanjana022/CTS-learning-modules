fda31d71708abef5297c0ecdb937fda6083b71d7
mohansanjana022 <mohansanjana022@gmail.com>
2025-05-25 14:18:37 +0530

    Update AverageSessionDurationperEvent.sql

--- ANSI SQL/AverageSessionDurationperEvent.sql
+++ ANSI SQL/AverageSessionDurationperEvent.sql
@@ -1,1 +1,13 @@
+SELECT 
+    e.event_id,
+    e.title AS event_title,
+    ROUND(AVG(TIMESTAMPDIFF(MINUTE, s.start_time, s.end_time)), 2) AS avg_session_duration_minutes
+FROM 
+    Events e
+JOIN 
+    Sessions s ON e.event_id = s.event_id
+GROUP BY 
+    e.event_id, e.title
+ORDER BY 
+    e.event_id;
 
