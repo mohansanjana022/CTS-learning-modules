491a123114bf6b0a095f85d12eab664b1e1ce59b
mohansanjana022 <mohansanjana022@gmail.com>
2025-05-25 13:21:40 +0530

    Update OrganizerEventSummary.sql

--- ANSI SQL/OrganizerEventSummary.sql
+++ ANSI SQL/OrganizerEventSummary.sql
@@ -1,1 +1,14 @@
+SELECT 
+    u.user_id,
+    u.full_name AS organizer_name,
+    e.status,
+    COUNT(e.event_id) AS event_count
+FROM 
+    Users u
+JOIN 
+    Events e ON u.user_id = e.organizer_id
+GROUP BY 
+    u.user_id, u.full_name, e.status
+ORDER BY 
+    u.user_id, e.status;
 
