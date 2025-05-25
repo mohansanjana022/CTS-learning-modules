f929636474ac6f4c60d474ea7fa85ce9e16f8b4e
mohansanjana022 <mohansanjana022@gmail.com>
2025-05-25 14:20:20 +0530

    Update EventsWithoutSessions.sql

--- ANSI SQL/EventsWithoutSessions.sql
+++ ANSI SQL/EventsWithoutSessions.sql
@@ -1,1 +1,14 @@
+SELECT 
+    e.event_id,
+    e.title AS event_title,
+    e.city,
+    e.start_date,
+    e.end_date,
+    e.status
+FROM 
+    Events e
+LEFT JOIN 
+    Sessions s ON e.event_id = s.event_id
+WHERE 
+    s.session_id IS NULL;
 
