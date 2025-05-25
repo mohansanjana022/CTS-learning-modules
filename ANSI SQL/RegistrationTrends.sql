cf54c81847071c6e7a922067aa167b03304c973c
mohansanjana022 <mohansanjana022@gmail.com>
2025-05-25 14:15:04 +0530

    Update RegistrationTrends.sql

--- ANSI SQL/RegistrationTrends.sql
+++ ANSI SQL/RegistrationTrends.sql
@@ -1,1 +1,12 @@
+SELECT 
+    DATE_FORMAT(registration_date, '%Y-%m') AS registration_month,
+    COUNT(*) AS registration_count
+FROM 
+    Registration
+WHERE 
+    registration_date >= DATE_SUB(CURDATE(), INTERVAL 12 MONTH)
+GROUP BY 
+    registration_month
+ORDER BY 
+    registration_month;
 
