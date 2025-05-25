3c46e38943bc3297ac41f7dd33abadae9d1d596c
mohansanjana022 <mohansanjana022@gmail.com>
2025-05-25 14:06:57 +0530

    Update UserEngagementIndex.sql

--- ANSI SQL/UserEngagementIndex.sql
+++ ANSI SQL/UserEngagementIndex.sql
@@ -1,1 +1,14 @@
+SELECT 
+    u.user_id,
+    u.full_name,
+    COUNT(DISTINCT r.event_id) AS events_attended,
+    COUNT(DISTINCT f.feedback_id) AS feedbacks_submitted
+FROM 
+    Users u
+LEFT JOIN 
+    Registration r ON u.user_id = r.user_id
+LEFT JOIN 
+    Feedback f ON u.user_id = f.user_id
+GROUP BY 
+    u.user_id, u.full_name;
 
